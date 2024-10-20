8e4a6992eb1c18f2b0246a0d56078385f7594e3f
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 12:18:36 +0530

    Update apk.js

--- plugins2/apk.js
+++ plugins2/apk.js
@@ -2,7 +2,7 @@ const config = require('../config')
 const { cmd, commands } = require('../command')
 cmd({
     pattern: "apk2",
-    alias: ["modapk"],
+    alias: ["app"],
     desc: "download apks",
     category: "download",
     react: "⚡",
