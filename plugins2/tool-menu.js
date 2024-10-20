561c472b732d0817c18e43ddfe31aa13e7c05238
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 15:17:36 +0530

    Update tool-menu.js

--- plugins2/tool-menu.js
+++ plugins2/tool-menu.js
@@ -1,1 +1,81 @@
-const _0xc3baab=_0x49eb;function _0x49eb(_0x1bf35d,_0x474692){const _0x5f0936=_0x5f09();return _0x49eb=function(_0x49ebfa,_0x38db8f){_0x49ebfa=_0x49ebfa-0xa8;let _0x3d02a2=_0x5f0936[_0x49ebfa];return _0x3d02a2;},_0x49eb(_0x1bf35d,_0x474692);}function _0x5f09(){const _0x1a9331=['log','get\x20bot\x20tool\x20cmd.','tools','dontAddCommandList','pattern','1273887GaHTGV','PREFIX','15Vvllug','28ctiDTe','../lib/database','\x0a╰───────────●●►\x0a\x20\x20','207660qKhafa','length','../command','1888976iDYYSe','https://i.ibb.co/YW8jpBJ/TOOL.png','category','3433760UrijbX','\x0a*👋\x20Hello\x20','9120744nqPBUL','12574pBrbME','sendMessage','3659424yOeSyC','*\x0a\x0a>\x20👨‍💻\x20​🇹​​🇭​​🇪​​🇳​​🇺​-​🇲​​🇩​\x20​🇲​​🇪​​🇳​​🇺​👨‍💻\x0a\x0a👨🏻‍💻⚟\x20*ɴᴀᴍᴇ\x20ʙᴏᴛ*\x20:\x20ᵀʰᵉⁿᵘ⁻ᴹᴰ💫\x0a👨🏻‍💻⚟\x20*ᴄʀᴇᴀᴛᴏʀ*\x20:\x20\x20𝓣𝓱𝓮𝓷𝓾𝓵𝓪\x20𝓟𝓪𝓷𝓪𝓹𝓲𝓽𝓲💫\x0a👨🏻‍💻⚟*ᴠᴇʀsɪᴏɴs*\x20:\x206.0.0\x20(ᴀᴅᴅᴇᴅ\x20ʜɪᴅᴅᴇɴ\x20ғᴇᴀᴜᴛᴜʀᴇs)💫\x0a👨🏻‍💻⚟\x20*ᴄᴏɴᴛᴀᴄᴛ*\x20:\x20https://wa.me/94757096717\x20💫\x0a\x0a╭───────────●●►\x0a│📜\x20TOOL\x20COMMANDS\x0a│───────\x0a'];_0x5f09=function(){return _0x1a9331;};return _0x5f09();}(function(_0x49ff6e,_0x4964cf){const _0x3e8776=_0x49eb,_0x322056=_0x49ff6e();while(!![]){try{const _0x591559=-parseInt(_0x3e8776(0xae))/0x1+-parseInt(_0x3e8776(0xbd))/0x2+parseInt(_0x3e8776(0xbf))/0x3+parseInt(_0x3e8776(0xba))/0x4+parseInt(_0x3e8776(0xb0))/0x5*(-parseInt(_0x3e8776(0xb4))/0x6)+parseInt(_0x3e8776(0xb1))/0x7*(-parseInt(_0x3e8776(0xb7))/0x8)+parseInt(_0x3e8776(0xbc))/0x9;if(_0x591559===_0x4964cf)break;else _0x322056['push'](_0x322056['shift']());}catch(_0x28f5ad){_0x322056['push'](_0x322056['shift']());}}}(_0x5f09,0xba524));const {readEnv}=require(_0xc3baab(0xb2)),{cmd,commands}=require(_0xc3baab(0xb6));cmd({'pattern':_0xc3baab(0xab),'desc':_0xc3baab(0xaa),'category':_0xc3baab(0xab),'react':'📩','filename':__filename},async(_0x10d49e,_0xfd480d,_0x474dab,{from:_0x3f27e1,quoted:_0x584407,body:_0x59baee,isCmd:_0x45921d,command:_0x38ad8b,args:_0x18cddf,q:_0x1b854f,isGroup:_0xb4694b,sender:_0x15e1ed,senderNumber:_0x9a8cc4,botNumber2:_0x46774c,botNumber:_0x206ae0,pushname:_0x1efca2,isMe:_0x82a770,isOwner:_0xe3ea52,groupMetadata:_0x54b4d4,groupName:_0x5a86e3,participants:_0x3cb68e,groupAdmins:_0x125665,isBotAdmins:_0x275464,isAdmins:_0x165c23,reply:_0x63dc09})=>{const _0x3b59a3=_0xc3baab;try{const _0xad8d22=await readEnv();let _0x44b98e={'tools':''};for(let _0x4835a8=0x0;_0x4835a8<commands[_0x3b59a3(0xb5)];_0x4835a8++){commands[_0x4835a8]['pattern']&&!commands[_0x4835a8][_0x3b59a3(0xac)]&&(_0x44b98e[commands[_0x4835a8][_0x3b59a3(0xb9)]]+=''+_0xad8d22[_0x3b59a3(0xaf)]+commands[_0x4835a8][_0x3b59a3(0xad)]+'\x0a');}let _0x2a595c=_0x3b59a3(0xbb)+_0x1efca2+_0x3b59a3(0xa8)+_0x44b98e[_0x3b59a3(0xab)]+_0x3b59a3(0xb3);await _0x10d49e[_0x3b59a3(0xbe)](_0x3f27e1,{'audio':{'url':'https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/refs/heads/main/plugins/bgm/AUD-20221018-WA0234.mp3'},'mimetype':'audio/mp4','ptt':!![]},{'quoted':_0xfd480d}),await _0x10d49e['sendMessage'](_0x3f27e1,{'image':{'url':_0x3b59a3(0xb8)},'caption':_0x2a595c},{'quoted':_0xfd480d});}catch(_0x58fd8b){console[_0x3b59a3(0xa9)](_0x58fd8b),_0x63dc09(''+_0x58fd8b);}});
+const { readEnv } = require('../lib/database'),
+  { cmd, commands } = require('../command')
+
+cmd(
+  {
+    pattern: 'tools',
+    desc: 'get bot tool cmd.',
+    category: 'tools',
+    react: '📩',
+    filename: __filename,
+  },
+  async (
+    bot,
+    message,
+    client,
+    {
+      from,
+      quoted,
+      body,
+      isCmd,
+      command,
+      args,
+      q,
+      isGroup,
+      sender,
+      senderNumber,
+      botNumber2,
+      botNumber,
+      pushname,
+      isMe,
+      isOwner,
+      groupMetadata,
+      groupName,
+      participants,
+      groupAdmins,
+      isBotAdmins,
+      isAdmins,
+      reply,
+    }
+  ) => {
+    try {
+      const env = await readEnv();
+      const commandList = {};
+
+      for (let i = 0; i < commands.length; i++) {
+        if (commands[i].pattern && !commands[i].dontAddCommandList) {
+          commandList[commands[i].category] =
+            (commandList[commands[i].category] || '') +
+            `${env.PREFIX}${commands[i].pattern}\n`;
+        }
+      }
+
+      let toolsMessage =
+        `\n👋 Hello ${pushname}\n\n> 👨‍💻 🇱🇰\n\n👨🏻‍💻⚙️ *Bot Name*: Queen D 💋\n👨🏻‍💻⚙️ *Creator*: Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🐱‍👤\n👨🏻‍💻⚙️ *Version*: 1.2.0 (Added features)💫\n👨🏻‍💻⚙️ *Insta*: https://www.instagram.com/yuren.sasanka/ 💫\n\n┌───────────────\n│📑 TOOL COMMANDS\n│───────────────\n${commandList.tools}\n└───────────────`;
+
+      await bot.sendMessage(
+        from,
+        {
+          audio: {
+            url: 'https://github.com/VajiraTech/IZUMI-AUTO-VOICER/raw/main/hi.mp3',
+          },
+          mimetype: 'audio/mp4',
+          ptt: true,
+        },
+        { quoted: message }
+      );
+
+      await bot.sendMessage(
+        from,
+        {
+          image: { url: 'https://i.ibb.co/Pr4BfhZ/1500x1500.png' },
+          caption: toolsMessage,
+        },
+        { quoted: message }
+      );
+    } catch (error) {
+      console.log(error);
+      reply(`${error}`);
+    }
+  }
+);
