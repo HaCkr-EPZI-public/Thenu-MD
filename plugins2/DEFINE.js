16d4ecd0076e4e66590a409669a3fe42bdc854a4
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 12:02:34 +0530

    Update DEFINE.js

--- plugins2/DEFINE.js
+++ plugins2/DEFINE.js
@@ -1,30 +1,58 @@
-//████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
-//╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
-//   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
-//   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
-//   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
-//   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const _0x49ffd7=_0x76d0;(function(_0x19d271,_0x336d04){const _0x293e3e=_0x76d0,_0x5afe62=_0x19d271();while(!![]){try{const _0x41e510=-parseInt(_0x293e3e(0x1b5))/0x1*(-parseInt(_0x293e3e(0x1a0))/0x2)+parseInt(_0x293e3e(0x19d))/0x3*(parseInt(_0x293e3e(0x1b2))/0x4)+-parseInt(_0x293e3e(0x19c))/0x5*(-parseInt(_0x293e3e(0x1ae))/0x6)+-parseInt(_0x293e3e(0x1a7))/0x7*(-parseInt(_0x293e3e(0x1ab))/0x8)+parseInt(_0x293e3e(0x1a9))/0x9*(-parseInt(_0x293e3e(0x1b7))/0xa)+parseInt(_0x293e3e(0x1b3))/0xb*(-parseInt(_0x293e3e(0x1a1))/0xc)+parseInt(_0x293e3e(0x1ad))/0xd;if(_0x41e510===_0x336d04)break;else _0x5afe62['push'](_0x5afe62['shift']());}catch(_0x535cca){_0x5afe62['push'](_0x5afe62['shift']());}}}(_0x3d2d,0x552d7));function _0x3d2d(){const _0x50e4d3=['164105Jgzuut','6kWMuYe','response','axios','997318MaGAHi','1834440heXahX','status','⚠️\x20An\x20error\x20occurred\x20while\x20fetching\x20the\x20definition.\x20Please\x20try\x20again\x20later.','https://api.dictionaryapi.dev/api/v2/entries/en/','../command','No\x20example\x20available','1631zsZjPS','\x0a🔗\x20*Synonyms*:\x20','5076CrRNCo','❗\x20Please\x20provide\x20a\x20word\x20to\x20define.\x20Usage:\x20.define\x20[word]','1696JVkzln','\x0a📚\x20*Word*:\x20','6461416KmoLzs','12rfadFe','\x0a📝\x20*Example*:\x20','log','meanings','49928RhfSGu','11UxzitG','\x0a\x0a>\x20❯❯\x20THENU-ᴍᴅ\x20ᴡᴀᴛꜱ\x20ᴀᴘᴘ\x20ʙᴏᴛ\x20➣','1HIOuPY','data','11240Mhidpy','join','No\x20synonyms\x20available','🚫\x20Word\x20not\x20found.\x20Please\x20check\x20the\x20spelling\x20and\x20try\x20again.','\x0a🔍\x20*Definition*:\x20','definitions','📚\x20Get\x20the\x20definition\x20of\x20a\x20word','get'];_0x3d2d=function(){return _0x50e4d3;};return _0x3d2d();}function _0x76d0(_0x3d82c1,_0xd23331){const _0x3d2dbc=_0x3d2d();return _0x76d0=function(_0x76d0a,_0x413b48){_0x76d0a=_0x76d0a-0x198;let _0x2a0fb9=_0x3d2dbc[_0x76d0a];return _0x2a0fb9;},_0x76d0(_0x3d82c1,_0xd23331);}const axios=require(_0x49ffd7(0x19f)),{cmd}=require(_0x49ffd7(0x1a5));cmd({'pattern':'define','desc':_0x49ffd7(0x19a),'react':'🔍','category':'search','filename':__filename},async(_0x271d43,_0x33b7d7,_0x2db1c5,{from:_0x2508f6,q:_0x1f742c,reply:_0x573c9b})=>{const _0x20ccf2=_0x49ffd7;try{if(!_0x1f742c)return _0x573c9b(_0x20ccf2(0x1aa));const _0x5beed6=_0x1f742c,_0x48a5a5=_0x20ccf2(0x1a4)+_0x5beed6,_0x39c7e6=await axios[_0x20ccf2(0x19b)](_0x48a5a5),_0x235d65=_0x39c7e6[_0x20ccf2(0x1b6)][0x0],_0x4facb8=_0x235d65[_0x20ccf2(0x1b1)][0x0]['definitions'][0x0]['definition'],_0x2dedb7=_0x235d65['meanings'][0x0][_0x20ccf2(0x199)][0x0]['example']||_0x20ccf2(0x1a6),_0x6051e=_0x235d65[_0x20ccf2(0x1b1)][0x0][_0x20ccf2(0x199)][0x0]['synonyms'][_0x20ccf2(0x1b8)](',\x20')||_0x20ccf2(0x1b9),_0x49c4a6=_0x20ccf2(0x1ac)+_0x235d65['word']+_0x20ccf2(0x198)+_0x4facb8+_0x20ccf2(0x1af)+_0x2dedb7+_0x20ccf2(0x1a8)+_0x6051e+_0x20ccf2(0x1b4);return _0x573c9b(_0x49c4a6);}catch(_0x575790){console[_0x20ccf2(0x1b0)](_0x575790);if(_0x575790[_0x20ccf2(0x19e)]&&_0x575790[_0x20ccf2(0x19e)][_0x20ccf2(0x1a2)]===0x194)return _0x573c9b(_0x20ccf2(0x1ba));return _0x573c9b(_0x20ccf2(0x1a3));}});
+const axios = require('axios'),
+  { cmd } = require('../command')
+cmd(
+  {
+    pattern: 'define',
+    desc: '\uD83D\uDCDA Get the definition of a word',
+    react: '\uD83D\uDD0D',
+    category: 'search',
+    filename: __filename,
+  },
+  async (
+    _0x271d43,
+    _0x33b7d7,
+    _0x2db1c5,
+    { from: _0x2508f6, q: _0x1f742c, reply: _0x573c9b }
+  ) => {
+    try {
+      if (!_0x1f742c) {
+        return _0x573c9b(
+          '\u2757 Please provide a word to define. Usage: .define [word]'
+        )
+      }
+      const _0x5beed6 = _0x1f742c,
+        _0x48a5a5 =
+          'https://api.dictionaryapi.dev/api/v2/entries/en/' + _0x5beed6,
+        _0x39c7e6 = await axios.get(_0x48a5a5),
+        _0x235d65 = _0x39c7e6.data[0],
+        _0x4facb8 = _0x235d65.meanings[0].definitions[0].definition,
+        _0x2dedb7 =
+          _0x235d65.meanings[0].definitions[0].example ||
+          'No example available',
+        _0x6051e =
+          _0x235d65.meanings[0].definitions[0].synonyms.join(', ') ||
+          'No synonyms available',
+        _0x49c4a6 =
+          '\n\uD83D\uDCDA *Word*: ' +
+          _0x235d65.word +
+          '\n\uD83D\uDD0D *Definition*: ' +
+          _0x4facb8 +
+          '\n\uD83D\uDCDD *Example*: ' +
+          _0x2dedb7 +
+          '\n\uD83D\uDD17 *Synonyms*: ' +
+          _0x6051e +
+          '\n\n> \u276F\u276F ©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ \u27A3'
+      return _0x573c9b(_0x49c4a6)
+    } catch (_0x575790) {
+      console.log(_0x575790)
+      if (_0x575790.response && _0x575790.response.status === 404) {
+        return _0x573c9b(
+          '\uD83D\uDEAB Word not found. Please check the spelling and try again.'
+        )
+      }
+      return _0x573c9b(
+        '\u26A0️ An error occurred while fetching the definition. Please try again later.'
+      )
+    }
+  }
+)
