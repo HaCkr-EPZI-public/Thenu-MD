cc8db2a9ad58ea2921b3005139d4afdbd87f2650
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 12:56:34 +0530

    Update dog.js

--- plugins2/dog.js
+++ plugins2/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *ʀᴀɴᴅᴏᴍ ᴅᴏɢ ɪᴍᴀɢᴇ*\n>Thenu-MD 🐶\n> ' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *ʀᴀɴᴅᴏᴍ ᴅᴏɢ ɪᴍᴀɢᴇ*\n> ©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🐶\n> ' }, { quoted: mek });
     } catch (e) {
         console.log(e); // ❯❯ Powered by Thenu-MD 👑
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
