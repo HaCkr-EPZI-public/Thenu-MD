2822e76c85a6f38727f026b26a5d726e9429624f
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 13:02:26 +0530

    Update fun-menu.js

--- plugins2/fun-menu.js
+++ plugins2/fun-menu.js
@@ -1,1 +1,78 @@
-function _0x2b48(){const _0x38c053=['2neAqIa','2208987OmuqTQ','pattern','11UpXOWs','fun','136436DlIUit','2192468iTTkvo','2761944jLhypQ','\x0a╰───────────●●►\x0a\x20\x20','110488RitXkn','category','78gACmPU','1741830GVhEtV','sendMessage','\x0a*👋\x20Hello\x20','../lib/database','12669050OLuDdi','5XXELcv','*\x0a\x0a>\x20👨‍💻\x20​🇹​​🇭​​🇪​​🇳​​🇺​-​🇲​​🇩​\x20​🇲​​🇪​​🇳​​🇺​👨‍💻\x0a\x0a👨🏻‍💻⚟\x20*ɴᴀᴍᴇ\x20ʙᴏᴛ*\x20:\x20ᵀʰᵉⁿᵘ⁻ᴹᴰ💫\x0a👨🏻‍💻⚟\x20*ᴄʀᴇᴀᴛᴏʀ*\x20:\x20\x20𝓣𝓱𝓮𝓷𝓾𝓵𝓪\x20𝓟𝓪𝓷𝓪𝓹𝓲𝓽𝓲💫\x0a👨🏻‍💻⚟*ᴠᴇʀsɪᴏɴs*\x20:\x206.0.0\x20(ᴀᴅᴅᴇᴅ\x20ʜɪᴅᴅᴇɴ\x20ғᴇᴀᴜᴛᴜʀᴇs)💫\x0a👨🏻‍💻⚟\x20*ᴄᴏɴᴛᴀᴄᴛ*\x20:\x20https://wa.me/94757096717\x20💫\x0a\x0a╭───────────●●►\x0a│📜\x20FUN\x20COMMANDS\x0a│───────\x0a','get\x20bot\x20fun\x20cmd.','PREFIX','https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/refs/heads/main/plugins/bgm/AUD-20221018-WA0236.mp3'];_0x2b48=function(){return _0x38c053;};return _0x2b48();}function _0x4da2(_0x557b50,_0x4daf15){const _0x2b48d3=_0x2b48();return _0x4da2=function(_0x4da27b,_0xa6bf92){_0x4da27b=_0x4da27b-0x1d5;let _0x4e2270=_0x2b48d3[_0x4da27b];return _0x4e2270;},_0x4da2(_0x557b50,_0x4daf15);}const _0x28e858=_0x4da2;(function(_0x1fd7fa,_0x4b16f5){const _0x406c57=_0x4da2,_0x13b01c=_0x1fd7fa();while(!![]){try{const _0x47ff6e=parseInt(_0x406c57(0x1e9))/0x1*(-parseInt(_0x406c57(0x1e4))/0x2)+-parseInt(_0x406c57(0x1da))/0x3+parseInt(_0x406c57(0x1ea))/0x4*(parseInt(_0x406c57(0x1df))/0x5)+-parseInt(_0x406c57(0x1d9))/0x6*(parseInt(_0x406c57(0x1d7))/0x7)+-parseInt(_0x406c57(0x1d5))/0x8+-parseInt(_0x406c57(0x1e5))/0x9+parseInt(_0x406c57(0x1de))/0xa*(parseInt(_0x406c57(0x1e7))/0xb);if(_0x47ff6e===_0x4b16f5)break;else _0x13b01c['push'](_0x13b01c['shift']());}catch(_0xa643a){_0x13b01c['push'](_0x13b01c['shift']());}}}(_0x2b48,0x49c12));const {readEnv}=require(_0x28e858(0x1dd)),{cmd,commands}=require('../command');cmd({'pattern':'fun','desc':_0x28e858(0x1e1),'category':_0x28e858(0x1e8),'react':'📩','filename':__filename},async(_0x4f09a4,_0x432604,_0x2ab794,{from:_0x158be1,quoted:_0x48259d,body:_0x165a66,isCmd:_0x460ed5,command:_0x33eda9,args:_0x59eaea,q:_0x369a9f,isGroup:_0xa155a3,sender:_0x193b9e,senderNumber:_0x28421f,botNumber2:_0x1109a7,botNumber:_0x4ba01f,pushname:_0x3226f3,isMe:_0x25c905,isOwner:_0x5c831c,groupMetadata:_0x181c87,groupName:_0x3cd111,participants:_0x934c1e,groupAdmins:_0x5a93a3,isBotAdmins:_0x314c2b,isAdmins:_0x147a11,reply:_0x37327b})=>{const _0x96c0f8=_0x28e858;try{const _0x3b232c=await readEnv();let _0x4da8b5={'fun':''};for(let _0x4f7080=0x0;_0x4f7080<commands['length'];_0x4f7080++){commands[_0x4f7080]['pattern']&&!commands[_0x4f7080]['dontAddCommandList']&&(_0x4da8b5[commands[_0x4f7080][_0x96c0f8(0x1d8)]]+=''+_0x3b232c[_0x96c0f8(0x1e2)]+commands[_0x4f7080][_0x96c0f8(0x1e6)]+'\x0a');}let _0x49c5c9=_0x96c0f8(0x1dc)+_0x3226f3+_0x96c0f8(0x1e0)+_0x4da8b5['fun']+_0x96c0f8(0x1d6);await _0x4f09a4[_0x96c0f8(0x1db)](_0x158be1,{'audio':{'url':_0x96c0f8(0x1e3)},'mimetype':'audio/mp4','ptt':!![]},{'quoted':_0x432604}),await _0x4f09a4[_0x96c0f8(0x1db)](_0x158be1,{'image':{'url':'https://i.ibb.co/c8gc7qD/FUN.png'},'caption':_0x49c5c9},{'quoted':_0x432604});}catch(_0x4ee9dd){console['log'](_0x4ee9dd),_0x37327b(''+_0x4ee9dd);}});
+const { readEnv } = require('../lib/database'),
+  { cmd, commands } = require('../command')
+cmd(
+  {
+    pattern: 'fun',
+    desc: 'get bot fun cmd.',
+    category: 'fun',
+    react: '\uD83D\uDCE9',
+    filename: __filename,
+  },
+  async (
+    _0x4f09a4,
+    _0x432604,
+    _0x2ab794,
+    {
+      from: _0x158be1,
+      quoted: _0x48259d,
+      body: _0x165a66,
+      isCmd: _0x460ed5,
+      command: _0x33eda9,
+      args: _0x59eaea,
+      q: _0x369a9f,
+      isGroup: _0xa155a3,
+      sender: _0x193b9e,
+      senderNumber: _0x28421f,
+      botNumber2: _0x1109a7,
+      botNumber: _0x4ba01f,
+      pushname: _0x3226f3,
+      isMe: _0x25c905,
+      isOwner: _0x5c831c,
+      groupMetadata: _0x181c87,
+      groupName: _0x3cd111,
+      participants: _0x934c1e,
+      groupAdmins: _0x5a93a3,
+      isBotAdmins: _0x314c2b,
+      isAdmins: _0x147a11,
+      reply: _0x37327b,
+    }
+  ) => {
+    try {
+      const _0x3b232c = await readEnv()
+      for (let _0x4f7080 = 0; _0x4f7080 < commands.length; _0x4f7080++) {
+        commands[_0x4f7080].pattern &&
+          !commands[_0x4f7080].dontAddCommandList &&
+          (_0x4da8b5[commands[_0x4f7080].category] +=
+            '' + _0x3b232c.PREFIX + commands[_0x4f7080].pattern + '\n')
+      }
+      let _0x49c5c9 =
+        '\n*\uD83D\uDC4B Hello ' +
+        _0x3226f3 +
+        '*\n\n> \uD83D\uDC68‍\uD83D\uDCBB \u200B\uD83C\uDDF9\u200B\u200B\uD83C\uDDED\u200B\u200B\uD83C\uDDEA\u200B\u200B\uD83C\uDDF3\u200B\u200B\uD83C\uDDFA\u200B-\u200B\uD83C\uDDF2\u200B\u200B\uD83C\uDDE9\u200B \u200B\uD83C\uDDF2\u200B\u200B\uD83C\uDDEA\u200B\u200B\uD83C\uDDF3\u200B\u200B\uD83C\uDDFA\u200B\uD83D\uDC68‍\uD83D\uDCBB\n\n\uD83D\uDC68\uD83C\uDFFB‍\uD83D\uDCBB\u269F *ɴᴀᴍᴇ ʙᴏᴛ* : Epzige cudu Nona🔮\n👨‍💻✨ *ᴄʀᴇᴀᴛᴏʀ* : ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🐱‍👤\n👨‍💻✨*ᴠᴇʀsɪᴏɴs* : 6.0.0 (ᴀᴅᴅᴇᴅ ʜɪᴅᴅᴇɴ ғᴇᴀᴜᴛᴜʀᴇs)\uD83D\uDCAB\n\uD83D\uDC68\uD83C\uDFFB‍\uD83D\uDCBB\u269F *ᴄᴏɴᴛᴀᴄᴛ* : https://wa.me/94757096717 \uD83D\uDCAB\n\n\u256D\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u25CF\u25CF\u25BA\n\u2502\uD83D\uDCDC FUN COMMANDS\n\u2502\u2500\u2500\u2500\u2500\u2500\u2500\u2500\n' +
+        '' +
+        '\n\u2570\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u25CF\u25CF\u25BA\n  '
+      await _0x4f09a4.sendMessage(
+        _0x158be1,
+        {
+          audio: {
+            url: 'https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/refs/heads/main/plugins/bgm/AUD-20221018-WA0236.mp3',
+          },
+          mimetype: 'audio/mp4',
+          ptt: true,
+        },
+        { quoted: _0x432604 }
+      )
+      await _0x4f09a4.sendMessage(
+        _0x158be1,
+        {
+          image: { url: 'https://i.ibb.co/Pr4BfhZ/1500x1500.png' },
+          caption: _0x49c5c9,
+        },
+        { quoted: _0x432604 }
+      )
+    } catch (_0x4ee9dd) {
+      console.log(_0x4ee9dd)
+      _0x37327b('' + _0x4ee9dd)
+    }
+  }
+)
