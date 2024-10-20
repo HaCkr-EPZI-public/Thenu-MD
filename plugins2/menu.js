49bdae9aa0e5599390a1b8f59a869900b6616843
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 15:26:52 +0530

    Update menu.js

--- plugins2/menu.js
+++ plugins2/menu.js
@@ -1,18 +1,72 @@
-//████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
-//╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
-//   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
-//   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
-//   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
-//   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
-                                                                   
+const { readEnv } = require('../lib/database'),
+  { cmd, commands } = require('../command')
 
+cmd(
+  {
+    pattern: 'menu',
+    react: '🗃️',
+    alias: ['panel'],
+    desc: "Get bot's command list.",
+    category: 'main',
+    use: '.menu',
+    filename: __filename,
+  },
+  async (
+    bot,
+    message,
+    client,
+    {
+      from,
+      quoted,
+      body,
+      isCmd,
+      command,
+      args,
+      q,
+      isGroup,
+      sender,
+      senderNumber,
+      botNumber2,
+      botNumber,
+      pushname,
+      isMe,
+      isOwner,
+      groupMetadata,
+      groupName,
+      participants,
+      groupAdmins,
+      isBotAdmins,
+      isAdmins,
+      reply,
+    }
+  ) => {
+    try {
+      let menuMessage =
+        `\n👋 Hello ${pushname}\n\n> 👨‍💻 🇱🇰\n\n👨🏻‍💻⚙️ *Bot Name*: Queen D 💋\n👨🏻‍💻⚙️ *Creator*: Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🐱‍👤\n👨🏻‍💻⚙️ *Version*: 1.2.0💫\n👨🏻‍💻⚙️ *Insta*: https://www.instagram.com/yuren.sasanka/ 💫\n\n*🔗 Click on the following commands to get full menu..*\n\n/////////////////////////////////////////////////////////\n\n> 📩 Get Download commands..\n\n*.downloads*\n\n> 💻 To Get MAIN commands..\n\n*.main*\n\n> 💎 To Get Group Commands..\n\n*.groupmenu*\n\n> ⚙️ To Get Owner Command..\n\n*.ownermenu*\n\n> 📢 To Get All Commands..\n\n*.panel*\n\n> 🤖 To Get AI COMMANDS...\n\n*.aimenu*\n\n> 🧰 To Get Tools Commands..\n\n*.tools*\n\n> 🛠️ To Get Convert Commands..\n\n*.convert*\n\n> 💥 To Get Search Commands..\n\n*.search*\n\n> To GET ECONOMY COMMANDS..\n\n*.economy*\n\n> 💕 To Get FUN COMMANDS..\n\n*.fun*\n\n> ©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ ..\n`;
 
+      await bot.sendMessage(
+        from,
+        {
+          audio: {
+            url: 'https://github.com/VajiraTech/IZUMI-AUTO-VOICER/raw/main/hi.mp3',
+          },
+          mimetype: 'audio/mp4',
+          ptt: true,
+        },
+        { quoted: message }
+      );
 
-
-
-
-
-
-
-
-const _0x18250d=_0x4b54;function _0x4b54(_0x3cacc3,_0x1f661d){const _0xe330e0=_0xe330();return _0x4b54=function(_0x4b540a,_0x3d5038){_0x4b540a=_0x4b540a-0x78;let _0x2c4b61=_0xe330e0[_0x4b540a];return _0x2c4b61;},_0x4b54(_0x3cacc3,_0x1f661d);}(function(_0x524480,_0x191453){const _0x30cfdf=_0x4b54,_0x400ab8=_0x524480();while(!![]){try{const _0x59c720=parseInt(_0x30cfdf(0x82))/0x1*(-parseInt(_0x30cfdf(0x8c))/0x2)+parseInt(_0x30cfdf(0x87))/0x3+parseInt(_0x30cfdf(0x84))/0x4*(parseInt(_0x30cfdf(0x7a))/0x5)+parseInt(_0x30cfdf(0x81))/0x6*(parseInt(_0x30cfdf(0x8f))/0x7)+parseInt(_0x30cfdf(0x88))/0x8+-parseInt(_0x30cfdf(0x7b))/0x9*(parseInt(_0x30cfdf(0x8d))/0xa)+parseInt(_0x30cfdf(0x8e))/0xb*(-parseInt(_0x30cfdf(0x89))/0xc);if(_0x59c720===_0x191453)break;else _0x400ab8['push'](_0x400ab8['shift']());}catch(_0x4afb85){_0x400ab8['push'](_0x400ab8['shift']());}}}(_0xe330,0xcd466));const {readEnv}=require(_0x18250d(0x80)),{cmd,commands}=require('../command');cmd({'pattern':_0x18250d(0x8b),'react':_0x18250d(0x7f),'alias':[_0x18250d(0x8a)],'desc':_0x18250d(0x7e),'category':_0x18250d(0x86),'use':'.menu','filename':__filename},async(_0x3634e5,_0x2c3e22,_0x419b25,{from:_0x2e31dd,quoted:_0x21f1ad,body:_0x33168c,isCmd:_0x31a06b,command:_0x409933,args:_0x4557c1,q:_0x41704a,isGroup:_0x1769d2,sender:_0x4e982a,senderNumber:_0x3317d7,botNumber2:_0x39170d,botNumber:_0x140f25,pushname:_0x1a0abc,isMe:_0x37dc89,isOwner:_0x34270c,groupMetadata:_0x14e3f8,groupName:_0x4267b5,participants:_0x210ba3,groupAdmins:_0x34b304,isBotAdmins:_0x175347,isAdmins:_0x2cc38c,reply:_0x3917c0})=>{const _0x30b924=_0x18250d;try{let _0xeb7553='\x0a*👋\x20Hello\x20'+_0x1a0abc+_0x30b924(0x85);return await _0x3634e5['sendMessage'](_0x2e31dd,{'audio':{'url':_0x30b924(0x7d)},'mimetype':_0x30b924(0x79),'ptt':!![]},{'quoted':_0x2c3e22}),await _0x3634e5[_0x30b924(0x78)](_0x2e31dd,{'image':{'url':_0x30b924(0x83)},'caption':_0xeb7553},{'quoted':_0x2c3e22});}catch(_0x3f5de2){console[_0x30b924(0x7c)](_0x3f5de2),_0x3917c0(''+_0x3f5de2);}});function _0xe330(){const _0x5998e2=['../lib/database','6236214wepjHB','227251nROzAY','https://i.ibb.co/FJ0W8qV/MENU.png','4EnAzef','*\x0a\x0a>\x20👨‍💻\x20​🇹​​🇭​​🇪​​🇳​​🇺​-​🇲​​🇩​\x20​🇲​​🇪​​🇳​​🇺​👨‍💻\x0a\x0a👨🏻‍💻⚟\x20*ɴᴀᴍᴇ\x20ʙᴏᴛ*\x20:\x20ᵀʰᵉⁿᵘ⁻ᴹᴰ💫\x0a👨🏻‍💻⚟\x20*ᴄʀᴇᴀᴛᴏʀ*\x20:\x20\x20𝓣𝓱𝓮𝓷𝓾𝓵𝓪\x20𝓟𝓪𝓷𝓪𝓹𝓲𝓽𝓲💫\x0a👨🏻‍💻⚟*ᴠᴇʀsɪᴏɴs*\x20:\x206.0.0💫\x0a👨🏻‍💻⚟\x20*ᴄᴏɴᴛᴀᴄᴛ*\x20:\x20https://wa.me/94757096717\x20💫\x0a\x0a*🔗𝚃𝚑𝚒𝚜\x20𝚒𝚜\x20𝚃𝚑𝚎𝚗𝚞-𝙼𝙳\x20𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜..*\x0a*🤦‍♂️Please\x20send\x20\x20FOLLOWING\x20Commands\x20to\x20get\x20full\x20menu..*\x0a\x0a/////////////////////////////////////////////////////////\x0a\x0a>\x20📩Get\x20Download\x20commands..\x0a\x0a*.𝚍𝚘𝚠𝚗𝚕𝚘𝚊𝚍*\x0a\x0a>\x20💻To\x20Get\x20MAIN\x20commands..\x0a\x0a*.main*\x0a\x0a>\x20💎To\x20GetGroup\x20Commands..\x0a\x0a*.groupmenu*\x0a\x0a>\x20⚙️To\x20Get\x20Owner\x20Command..\x0a\x0a*.ownermenu*\x0a\x0a>\x20📢To\x20Get\x20All\x20Commands..\x0a\x0a*.panel*\x0a\x0a>\x20🤖TO\x20GET\x20AI\x20COMMANDS...\x0a\x0a*.aimenu*\x0a\x0a>\x20🧰To\x20Get\x20Tools\x20Commands..\x0a\x0a*.tools*\x0a\x0a>\x20🛡️To\x20Get\x20Convert\x20\x20Commands..\x0a\x0a*.convert*\x0a\x0a>\x20💣To\x20Get\x20Search\x20Commands..\x0a\x0a*.search*\x0a\x0a>\x20To\x20GET\x20ECONOMY\x20COMMANDS..\x0a\x0a*.economy*\x0a\x0a>\x20💕To\x20Get\x20FUN\x20COMMANDS..\x0a\x0a*.fun*\x0a\x0a>\x20ᑭOᗯEᖇEᗪ\x20ᗷY\x20TᕼEᑎᑌᒪᗩ\x20ᑭᗩᑎᗩᑭITI..\x0a','main','4637097PYdWiP','5255488LTcdOJ','153516KeeJmF','panel','menu','2vFyTgb','15082310fjQnQc','1441apKoBk','7rHwnJm','sendMessage','audio/mp4','5050835ACeiXj','9YHZlhs','log','https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/refs/heads/main/plugins/bgm/AUD-20221018-WA0207.mp3','Get\x20bot\x27s\x20command\x20list.','🗃️'];_0xe330=function(){return _0x5998e2;};return _0xe330();}
+      await bot.sendMessage(
+        from,
+        {
+          image: { url: 'https://i.ibb.co/Pr4BfhZ/1500x1500.png' },
+          caption: menuMessage,
+        },
+        { quoted: message }
+      );
+    } catch (error) {
+      console.log(error);
+      reply(`${error}`);
+    }
+  }
+);
