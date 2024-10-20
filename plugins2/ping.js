63b58a948da829037fc01f33f00fd09065f6ce04
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 14:19:05 +0530

    Update ping.js

--- plugins2/ping.js
+++ plugins2/ping.js
@@ -11,10 +11,10 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '> THENU-MD...' })
+        const message = await conn.sendMessage(from, { text: '> Queen D...' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `📍 Ping : ${ping}ms`, quoted: message })
+        await conn.sendMessage(from, { text: `Epzi's Speed : ${ping}ms`, quoted: message })
      }catch(e){
     console.log(e)
     reply(`${e}`)
